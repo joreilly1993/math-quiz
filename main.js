42baae01be1338e648840378a1e82d66f6a32d69
John O'Reilly <johnoreilly.cny@gmail.com>
2021-11-14 18:39:21 -0500

    Editied main.js, sixes.html and styles.css. All the quiz question divs are now added via the DOM and in a random order. Updated the number of quiz questions from 10 to 20 to include *x6's as well as 6x*'s.

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ gradeDiv.classList.add('grade-styling');
 let percentageScore = document.createElement('div');
 percentageScore.classList.add('grade-styling');
 
+
+
+
+function tryAgainBtn() {
+    let tryAgainBtn = document.createElement('button');
+    tryAgainBtn.textContent = "Reset Questions";
+    tryAgainBtn.classList.add('try-again-btn');
+    gradeDiv.appendChild(tryAgainBtn);
+
+      tryAgainBtn.addEventListener('click', (e) => {
+        location.reload();
+      });
+};
+
 function percentage() {
     gradeDiv.appendChild(percentageScore);
     if (studentGrade === 0) {
@@ -43,30 +57,116 @@ function percentage() {
 }
 
 
-let firstSixSubmit = document.getElementById('first-six-btn');
-let firstBtnDiv = document.getElementById('six-first')
-let secondSixSubmit = document.getElementById('second-six-btn');
-let secondBtnDiv = document.getElementById('six-second');
-let thirdSixSubmit = document.getElementById('third-six-btn');
-let thirdBtnDiv = document.getElementById('six-third');
-let fourthSixSubmit = document.getElementById('fourth-six-btn');
-let fourthBtnDiv = document.getElementById('six-fourth');
-let fifthSixSubmit = document.getElementById('fifth-six-btn');
-let fifthBtnDiv = document.getElementById('six-fifth');
-let sixthSixSubmit = document.getElementById('sixth-six-btn');
-let sixthBtnDiv = document.getElementById('six-sixth');
-let seventhSixSubmit = document.getElementById('seventh-six-btn');
-let seventhBtnDiv = document.getElementById('six-seventh');
-let eighthSixSubmit = document.getElementById('eighth-six-btn');
-let eighthBtnDiv = document.getElementById('six-eighth');
-let ninthSixSubmit = document.getElementById('ninth-six-btn');
-let ninthBtnDiv = document.getElementById('six-ninth');
-let tenthSixSubmit = document.getElementById('tenth-six-btn');
-let tenthBtnDiv = document.getElementById('six-tenth');
 
 
 
 
+let firstBtnDiv = document.createElement('div');
+firstBtnDiv.textContent = 'Div 1';
+
+let secondBtnDiv = document.createElement('div');
+secondBtnDiv.textContent = 'Div 2';
+
+let thirdBtnDiv = document.createElement('div');
+thirdBtnDiv.textContent = 'Div 3';
+
+let fourthBtnDiv = document.createElement('div');
+fourthBtnDiv.textContent = 'Div 4';
+
+let fifthBtnDiv = document.createElement('div');
+fifthBtnDiv.textContent = 'Div 5';
+
+let sixthBtnDiv = document.createElement('div');
+sixthBtnDiv.textContent = 'Div 6';
+
+let seventhBtnDiv = document.createElement('div');
+seventhBtnDiv.textContent = 'Div 7';
+
+let eighthBtnDiv = document.createElement('div');
+eighthBtnDiv.textContent = 'Div 8';
+
+
+let ninthBtnDiv = document.createElement('div');
+ninthBtnDiv.textContent = 'Div 9';
+
+
+let tenthBtnDiv = document.createElement('div');
+tenthBtnDiv.textContent = 'Div 10';
+
+
+
+
+
+
+let firstTimesSix = document.createElement('div');
+firstTimesSix.textContent = 'Div 11';
+
+let secondTimesSix = document.createElement('div');
+secondTimesSix.textContent = 'Div 12';
+
+let thirdTimesSix = document.createElement('div');
+thirdTimesSix.textContent = 'Div 13';
+
+let fourthTimesSix = document.createElement('div');
+fourthTimesSix.textContent = 'Div 14';
+
+let fifthTimesSix = document.createElement('div');
+fifthTimesSix.textContent = 'Div 15';
+
+let sixthTimesSix = document.createElement('div');
+sixthTimesSix.textContent = 'Div 16';
+
+let seventhTimesSix = document.createElement('div');
+seventhTimesSix.textContent = 'Div 17';
+
+let eighthTimesSix = document.createElement('div');
+eighthTimesSix.textContent = 'Div 18';
+
+
+let ninthTimesSix = document.createElement('div');
+ninthTimesSix.textContent = 'Div 19';
+
+
+let tenthTimesSix = document.createElement('div');
+tenthTimesSix.textContent = 'Div 20';
+
+
+
+
+let container = document.querySelector('#container');
+let quizQuestions = document.querySelector('#quiz-questions');
+let sixTimesDiv = document.createElement('div');
+sixTimesDiv.classList.add('border');
+let timesSixDiv = document.createElement('div');
+timesSixDiv.classList.add('border');
+quizQuestions.appendChild(sixTimesDiv);
+quizQuestions.appendChild(timesSixDiv);
+
+
+
+let sixTimesArr = [firstBtnDiv, secondBtnDiv, thirdBtnDiv, fourthBtnDiv, fifthBtnDiv, sixthBtnDiv, seventhBtnDiv, eighthBtnDiv, ninthBtnDiv, tenthBtnDiv];
+
+sixTimesArr.sort(() => Math.random() - 0.5);
+
+
+for (let i=0; i<sixTimesArr.length; i++) {
+    sixTimesDiv.appendChild(sixTimesArr[i]);
+}
+
+
+
+let timesSixArr = [firstTimesSix, secondTimesSix, thirdTimesSix, fourthTimesSix, fifthTimesSix, sixthTimesSix, seventhTimesSix, eighthTimesSix, ninthTimesSix, tenthTimesSix];
+
+timesSixArr.sort(() => Math.random() - 0.5);
+
+
+for (let i=0; i<timesSixArr.length; i++) {
+    timesSixDiv.appendChild(timesSixArr[i]);
+}
+
+
+
+/*
 firstSixSubmit.addEventListener('click', (e) => {
     let firstSixInput = document.getElementsByTagName("input")[0].value;
     console.log(firstSixInput);
@@ -84,6 +184,7 @@ firstSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         
     } 
     else if (firstSixInput.length === 0) {
@@ -98,7 +199,11 @@ firstSixSubmit.addEventListener('click', (e) => {
         firstBtnDiv.appendChild(incorrectImg);
         document.getElementById('first-six-input').disabled = true;
         firstSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -119,6 +224,7 @@ secondSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     } 
     else if (secondSixInput.length === 0) {
     }
@@ -132,7 +238,11 @@ secondSixSubmit.addEventListener('click', (e) => {
         secondBtnDiv.appendChild(incorrectImg);
         document.getElementById('second-six-input').disabled = true;
         secondSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -153,6 +263,7 @@ thirdSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     } 
     else if (thirdSixInput.length === 0) {
     }
@@ -166,7 +277,11 @@ thirdSixSubmit.addEventListener('click', (e) => {
         thirdBtnDiv.appendChild(incorrectImg);
         document.getElementById('third-six-input').disabled = true;
         thirdSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -188,6 +303,7 @@ fourthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (fourthSixInput.length === 0) {
         }
@@ -201,7 +317,11 @@ fourthSixSubmit.addEventListener('click', (e) => {
         fourthBtnDiv.appendChild(incorrectImg);
         document.getElementById('fourth-six-input').disabled = true;
         fourthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -223,6 +343,7 @@ fifthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (fifthSixInput.length === 0) {
         }
@@ -236,7 +357,11 @@ fifthSixSubmit.addEventListener('click', (e) => {
         fifthBtnDiv.appendChild(incorrectImg);
         document.getElementById('fifth-six-input').disabled = true;
         fifthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -258,6 +383,7 @@ sixthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (sixthSixInput.length === 0) {
         }
@@ -271,7 +397,11 @@ sixthSixSubmit.addEventListener('click', (e) => {
         sixthBtnDiv.appendChild(incorrectImg);
         document.getElementById('sixth-six-input').disabled = true;
         sixthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -293,6 +423,7 @@ seventhSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (seventhSixInput.length === 0) {
         }
@@ -306,7 +437,11 @@ seventhSixSubmit.addEventListener('click', (e) => {
         seventhBtnDiv.appendChild(incorrectImg);
         document.getElementById('seventh-six-input').disabled = true;
         seventhSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -328,6 +463,7 @@ eighthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (eighthSixInput.length === 0) {
         }
@@ -341,7 +477,11 @@ eighthSixSubmit.addEventListener('click', (e) => {
         eighthBtnDiv.appendChild(incorrectImg);
         document.getElementById('eighth-six-input').disabled = true;
         eighthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -363,6 +503,7 @@ ninthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (ninthSixInput.length === 0) {
         }
@@ -376,7 +517,11 @@ ninthSixSubmit.addEventListener('click', (e) => {
         ninthBtnDiv.appendChild(incorrectImg);
         document.getElementById('ninth-six-input').disabled = true;
         ninthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
 
@@ -398,6 +543,7 @@ tenthSixSubmit.addEventListener('click', (e) => {
         gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
         titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
         }
         else if (tenthSixInput.length === 0) {
         }
@@ -411,9 +557,13 @@ tenthSixSubmit.addEventListener('click', (e) => {
         tenthBtnDiv.appendChild(incorrectImg);
         document.getElementById('tenth-six-input').disabled = true;
         tenthSixSubmit.disabled = true;
+        studentGrade = studentGrade + 0;
+        gradeDiv.textContent = 'Grade: ' + studentGrade + '/10';
+        titleAndGrade.appendChild(gradeDiv);
         percentage();
+        tryAgainBtn();
     }
 });
-
+*/
 
 
